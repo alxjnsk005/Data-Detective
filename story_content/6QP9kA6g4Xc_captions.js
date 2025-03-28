d4c344d522ea93426733d2dd1a642c2863ae164d
alxjnsk005 <alxjnsk005@gmail.com>
2025-03-28 14:22:13 -0400

    2025-03-28

---
+++ story_content/6QP9kA6g4Xc_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34263.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.965%0D%0AOnce%20you%20are%20finished,%20click%20on%20the%20%E2%80%9CFinish%E2%80%9D%20button%20to%20exit%20the%20activity.%0D%0A%0D%0A"}')
